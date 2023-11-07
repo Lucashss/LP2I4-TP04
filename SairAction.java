925fe5126784a764eb070237d815d5cf87f0b390
Lucas Henrique <72462149+Lucashss@users.noreply.github.com>
2023-11-07 00:10:12 -0300

    Add files via upload

---
+++ SairAction.java
@@ -0,0 +1,14 @@
+/**
+ * @author LUCAS HENRIQUE e KETHELEEN CRISTINE
+ */
+
+import java.awt.event.*;
+
+public class SairAction
+    extends WindowAdapter
+{
+    public void windowClosing(WindowEvent e)
+    {
+        System.exit(0);
+    }
+}
